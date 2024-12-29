369c91dbdb94b31da2c041183a8b7acc47edb611
Marcos Rosada <marcos.rosada@digitalhub.man>
2024-12-29 23:29:44 +0000

    feat(Application): First Commit

---
+++ src/main/kotlin/com/devtiro/bookstore/BookStoreApplication.kt
@@ -0,0 +1,11 @@
+package com.devtiro.bookstore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookStoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<BookStoreApplication>(*args)
+}
